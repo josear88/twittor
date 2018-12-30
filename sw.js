060ecd1ab5c91484f13c48b5c80b525dc5908227
Jose Arenas <josearenas_88@outlook.com>
2018-12-29 20:42:19 -0500

    caches

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/sw-utils.js');
 
 
 const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -64,6 +64,11 @@ self.addEventListener('activate', e => {
 
             if( key !== STATIC_CACHE && key.includes('static')){
                 
+                return caches.delete(key);
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+
                 return caches.delete(key);
             }
 
